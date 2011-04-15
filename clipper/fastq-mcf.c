1ded09af1f10f3dbea0ff08610ef3b4d450f4a19
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2011-04-15 20:12:52 +0000

    added license stuff

--- clipper/fastq-mcf.c
+++ clipper/fastq-mcf.c
@@ -1,3 +1,31 @@
+/*
+Copyright (c) 2011 Expression Analysis / Erik Aronesty
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/* 
+
+See "void usage" below for usage.
+
+*/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,13 +36,6 @@
 #include <math.h>
 #include <limits.h>
 #include <sys/stat.h>
-
-/*
-
-Currently only works with adapter sequences that are at the END of read lines.
-See usage below.
-
-*/
 
 #define MAX_ADAPTER_NUM 1000
 #define max(a,b) (a>b?a:b)
