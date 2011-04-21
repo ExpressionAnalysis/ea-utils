59b439cf78517ab4d5f57baf74c5545fd49ff3ed
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2011-04-21 20:23:58 +0000

    remove the part that randomly breaks

--- clipper/install.sh
+++ clipper/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -e
 
-g++ -O3 fastq-clipper.c -o /opt/bin/fastq-clipper
-g++ -O3 fastq-mcf.c -o /opt/bin/fastq-mcf
-g++ -O3 fastq-multx.c -o /opt/bin/fastq-multx
-g++ -O3 fastq-join.c -o /opt/bin/fastq-join
+path=`cat install.path`
+
+g++ -O3 fastq-clipper.c -o $path/fastq-clipper&
+g++ -O3 fastq-mcf.c -o $path/fastq-mcf&
+g++ -O3 fastq-multx.c -o $path/fastq-multx&
+g++ -O3 fastq-join.c -o $path/fastq-join&
+wait
+
+echo OK
