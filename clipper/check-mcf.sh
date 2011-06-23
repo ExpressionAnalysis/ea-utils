a8fa4faeb81fa43c79e8a3e078f89afbf312e564
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2011-06-23 16:08:00 +0000

    add CHANGES file to keep track of major differences

--- clipper/check-mcf.sh
+++ clipper/check-mcf.sh
@@ -25,10 +25,11 @@ for v in $list; do
 	${prog} test.fa test1.fq > test1.$v.out 2> test1.$v.err
 	${prog} test.fa test2.fq > test2.$v.out 2> test2.$v.err
 	${prog} test.fa test1.fq -o test3.$v.out > test3.$v.err
+	${prog} -L 72 -f test.fa test4.fq1 test4.fq2 -o test4.$v.out -o test4.$v.out2
 done
 set +o xtrace	
 
-for n in test1 test2 test3; do
+for n in test1 test2 test3 test4; do
 	echo $n
 	diff $n.new.out $n.ok.out
 	[[ -e $n.ok.err ]] && diff $n.new.err $n.ok.err
