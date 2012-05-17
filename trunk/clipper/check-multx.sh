0c3e8a6de1ac4c7362150608a35ba99ae55b2331
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2012-05-17 19:30:54 +0000

    added test

--- trunk/clipper/check-multx.sh
+++ trunk/clipper/check-multx.sh
@@ -5,6 +5,8 @@ g++ fastq-multx.c fastq-lib.cpp && ./a.out -v ' ' -l /mnt/scratch/all_barcodes.t
 
 g++ fastq-multx.c fastq-lib.cpp && ./a.out -v ' ' -l /mnt/scratch/all_barcodes.txt mxtest_2.fastq mxtest_1.fastq mxtest_3.fastq -o n/a -o mxout_%_1.fq.tmp.gz -o mxout_%_2.fq.tmp.gz > t2
 
+g++ -g fastq-multx.c fastq-lib.cpp && ./a.out -g mxtest_2.fastq mxtest_1.fastq mxtest_3.fastq -o mzout_%_1.fq.tmp -o mzout_%_2.fq.tmp > t3
+
 for x in LB2 LB4 LB5 LB6; do
 	if ( ! diff  mxout_${x}_1.fq.tmp  mxout_${x}_1.fq > /dev/null ); then
 		echo FAIL: diff  mxout_${x}_1.fq.tmp  mxout_${x}_1.fq
@@ -12,13 +14,13 @@ for x in LB2 LB4 LB5 LB6; do
 	fi
 done
 
-for x in t1 t2; do
+for x in t1 t2 t3; do
 if ( ! diff  mxout.$x.txt $x ); then
-	echo FAIL: diff t1 mxout.txt
+	echo FAIL: diff $x mxout.$x.txt
 	exit 1
 fi
 done
 
-rm mx*.tmp mx*.tmp.gz t1 t2
+rm mz*.tmp mx*.tmp mx*.tmp.gz t1 t2 t3
 
 echo OK
