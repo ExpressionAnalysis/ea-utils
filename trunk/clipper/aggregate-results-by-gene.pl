b432676cd577977abcfae6edb7e1da6f175944c8
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2013-06-26 14:39:54 +0000

    no deps

--- trunk/clipper/aggregate-results-by-gene.pl
+++ trunk/clipper/aggregate-results-by-gene.pl
@@ -24,8 +24,9 @@
 
 use strict;
 use Getopt::Long;
-use EA;
+use IO::File;
 use Data::Dumper;
+use Carp qw(croak);
 
 my %opt;
 GetOptions(\%opt, "gtf=s") || die usage();
@@ -121,6 +122,173 @@ usage: $0 -g gtf_file IN1 IN2 ...
 
 Aggregates isoform counts by the gene information in a GTF file gene info
 
+Works with express-1.X output, or ea-utils/sam-stats coverage-matrix output.
+
 EOF
+}
+
+
+# this ia a really cool function that turns a csv file into a hash table
+# it's not "complete" but it works well... and at least i understand it
+# usage: xsvparse($file, option1=>val, option1=>val)
+#
+# all options are optional... by defualt it guesses the quote char, and the delimiter, and returns an array of hashes
+# if you specify a key, then it will return a hash of hashes indexed by that key
+# for example xsvparse("Summary.tsv", key=lane);
+
+# OPTIONS:
+#   nocase  BOOL      : lowercases all keys and field names
+#   skip    INT|REGEX : skips lines of file before reading
+#   fields  AREF      : reference to an array of field names, for files without headers
+#   key     NAME      : if specified, you get a hash of hashes indexed by a key field
+#   delim   TEXT      : if specified, the delimiter to use (otherwise autodetect)
+#   quot    TEXT      : if specified, the quot char to use (otherwise autodetect)
+#   multi   BOOL      : if specified, each keyed entry is an arrayref of all matches
+#   sub     FUNCREF   : if specified, each row is passed to the funcref
+
+
+sub xsvparse {
+    my ($file, %op) = @_;
+
+    my $in = new IO::File;
+    my $fin = $file =~ /\.gz$/ ? "gunzip -c $file|" : $file;
+    open ($in, $fin) || return undef;
+
+    lcasehash(\%op);
+
+    my $l1;
+
+    if (!$op{delim}) {
+        $l1 = <$in>;
+        $l1 =~ s/[\r\n]+$//o;
+    }
+
+    my $skip_me = sub {
+        if ($op{skip}) {
+            if (ref($op{skip}) eq 'Regexp') {
+                do {
+                    $l1 = <$in>; $l1 =~ s/[\r\n]+$//o;
+                } while ($l1 =~ $op{skip});
+            } else {
+                my $skip = $op{skip};
+                while ($skip > 0) {
+                    $l1 = <$in>; $l1 =~ s/[\r\n]+$//o;
+                    --$skip;
+                }
+            }
+        }
+    };
+
+    &$skip_me();
+
+    return undef unless $l1 || $op{delim};
+
+    local $_;
+
+    if (!$op{delim}) {
+        my $n = 0;
+        my $d;
+        for ("\t", ",", "|") {
+            my $c = $_ eq "\t" ? $l1 =~ tr/\t// : $_ eq "," ?  $l1 =~ tr/,// : $l1 =~ tr/|//;
+            if ($c > $n) {$d = $_; $n = $c};
+        }
+        $op{delim} = $d if $n > 0;
+        croak "xsvparse can't determine delimiter for $file" if $n == 0;
+        if ($op{fields}) {
+            close($in);
+            open($in, $fin);
+            &$skip_me();
+        }
+    }
+
+    my $rxm = undef;
+    if (!$op{quot}) {
+        my $n = $l1 =~ tr/\"//;
+        if ($n > 1) {
+            $op{quot} = '"';
+        }
+    }
+    if ($op{quot}) {
+        # quoted csv parser... slower, but works fine
+        $rxm = qr{$op{quot}([^$op{quot}\\]*(?:\\.[^$op{quot}\\]*)*)$op{quot}$op{delim}?|([^$op{delim}]+)$op{delim}?|$op{delim}};
+    }
+    if ($op{fields}) {
+        if (!ref($op{fields}) eq 'ARRAY') {
+            die "list of field names should be an array";
+        }
+    } else {
+        if (!$op{fields}) {
+            $l1 = <$in> if ! defined $l1;
+            if ($rxm) {
+                @{$op{fields}} = ();
+                push(@{$op{fields}} ,$+) while $l1 =~ m/$rxm/gx;
+            } else {
+                $op{fields} = [split($op{delim}, $l1)];
+                map {s/\s+$//} @{$op{fields}};
+            }
+        }
+        if ($op{rnames}) {
+            $op{fields} = ['<key>', @{$op{fields}}] unless $op{fields}->[0] eq '';
+            $op{fields}->[0]=$op{key}='<key>';
+        }
+        if ($op{nocase}) {
+            for (@{$op{fields}}) {
+                $_=lc($_);
+            }
+        }
+    }
+    if ($op{nocase} && $op{key}) {
+        $op{key} = lc($op{key});
+    }
+
+    no warnings;
+    my $r;
+    my @d;
+    my @d;
+    while (<$in>) {
+        s/[\r\n]+$//o;
+        if ($rxm) {
+            @d = ();
+            push(@d ,$+) while m/$rxm/gx;
+            push(@d,undef) if substr($_, -1,1) eq $op{delim};
+        } else {
+            @d = split($op{delim}, $_);
+        }
+        my %h;
+        for (my $i = 0; $i < @{$op{fields}}; ++$i) {
+            $h{$op{fields}->[$i]}=$d[$i];
+        }
+        if ($op{sub}) {
+            last if &{$op{sub}}(\%h) eq 'last';
+        } elsif ($op{key}) {
+            if ($op{nocase}) {
+                if ($op{multi}) {
+                    push @{$r->{lc($h{$op{key}})}}, \%h;
+                } else {
+                    $r->{lc($h{$op{key}})}=\%h;
+                }
+            } else {
+                if ($op{multi}) {
+                    push @{$r->{$h{$op{key}}}}, \%h;
+                } else {
+                    $r->{$h{$op{key}}}=\%h;
+                }
+            }
+        } else {
+            push @{$r}, \%h;
+        }
+    }
+
+    return $r;
+}
+
+sub lcasehash {
+    my $h = shift;
+        for (keys(%$h)) {
+                if (! $_ eq lc($_) ) {
+                        $h->{lc($_)}=$h->{$_};
+                        delete $h->{$_};
+                }
+        }
 }
 
