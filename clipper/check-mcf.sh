dac9660614ba40bf01a52410d179ac4949801aa7
earonesty <earonesty@f1db881f-09c6-4a82-8334-ae7ceabc6398>
2014-06-05 14:54:48 +0000

    include Command remove

--- clipper/check-mcf.sh
+++ clipper/check-mcf.sh
@@ -11,6 +11,13 @@ err()
 	err=1	
 } 
 
+runtmp ()
+{
+	grep -v Command $2 > a.tmp 
+    grep -v Command $3 > b.tmp
+	$1 a.tmp b.tmp 1>&2
+}
+
 if [ "$1" = "-b" ]; then
 	list="new ok"
 else
@@ -23,6 +30,7 @@ set -o xtrace
 for v in $list; do
 	eval prog=\$$v
 	${prog} -l 15 test.fa test1.fq > test1.$v.out 2> test1.$v.err || echo err during 1 $v
+    echo should be error during 2...
 	${prog} -l 15 test.fa test2.fq > test2.$v.out 2> test2.$v.err || echo err during 2 $v
 	${prog} -l 15 test.fa test1.fq -o test3.$v.out > test3.$v.err || echo err during 3 $v
 	${prog} -l 15 -L 72 -f test.fa test4.fq1 test4.fq2 -o test4.$v.out -o test4.$v.out2 > /dev/null || echo err during 4 $v
@@ -36,23 +44,23 @@ done
 set +o xtrace	
 
 for n in test1 test2 test3 test4 test5 test7; do
-	echo $n
-	diff $n.new.out $n.ok.out
-	[[ -e $n.ok.err ]] && diff $n.new.err $n.ok.err
+	echo diff $n.new.out $n.ok.out
+	diff $n.new.out $n.ok.out 1>&2
+	[[ -e $n.ok.err ]] && runtmp diff $n.new.err $n.ok.err 1>&2
 done
 
 n=test6
-echo $n
+echo special: $n
 zdiff $n.new.out.gz $n.ok.out.gz
 
 shopt -s extglob
-
-rm test?.@(new).@(out|err)
 
 if [ $err == 1 ]; then
 	echo NOT OK, some differences
 	exit 1
 else
+    rm test?.@(new).@(out|err)
+    rm a.tmp b.tmp
 	echo OK, all tests passed
 	exit 0
 fi
